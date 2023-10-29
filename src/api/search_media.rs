5dcbf58aa00ab0ca0b4d7108bef1ffbd90740a13
Tom <oerushia@gmail.com>
2023-10-29 07:32:42 +0000

    ANILIST COMMAND: Will not work with URL, Thumb or Image?

--- src/api/search_media.rs
+++ src/api/search_media.rs
@@ -1,42 +1,58 @@
 use std::collections::HashMap;
 use crate::api::queries::get_query;
 use tracing::info;
-use serde_json::json;
 
-pub async fn search(media_name: String, media_type: String) -> Vec<String> {
+pub async fn search(media_name: String, media_type: String) -> (Vec<String>, Vec<String>) {
     info!("Searching for {} in {}", media_name, media_type);
     let client = reqwest::Client::new();
     let mut map = HashMap::new();
     let query = get_query("search");
 
+
+    // Insert GrahpQL Query and Variables
+    // Query: Sarch
+    // Variables: "Search": Anime_Name, "Type": Anime_Type
     map.insert("query", query);
     map.insert("variables", format!("{{\"search\": \"{}\", \"type\": \"{}\"}}", media_name, media_type.to_uppercase()));
     info!("Inserted values into map");
 
     let res = client.post("https://graphql.anilist.co")
-                                    .json(&map)
-                                    .send()
-                                    .await;
+                                        .json(&map)
+                                        .send()
+                                        .await;
 
     info!("Sent request to AniList");
-    let res = json![res.unwrap().json::<serde_json::Value>().await.unwrap()];
+    let res = res.unwrap().json::<serde_json::Value>().await.unwrap();
     let data = &res["data"]["Media"];
     info!("Received response from AniList");
 
     // Anime Information
     let title = &data["title"]["romaji"];
-    let format = &data["format"];
     let status = &data["status"];
     let episodes = &data["episodes"];
-    let duration = &data["duration"];
     let genres = &data["genres"];
     let mean_score = &data["meanScore"];
+    let average_score = &data["averageScore"];
     let popularity = &data["popularity"];
-    let season = &data["season"];
+    let favourites = &data["favourites"];
     let url = &data["siteUrl"];
+    let avatar = &data["coverImage"]["extraLarge"];
+    let banner = &data["bannerImage"];
 
-    vec![title, format, status, episodes, duration, genres, mean_score, popularity, season, url]
-        .iter()
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>()
+    info!("Returning Information For {}", title);
+    (vec![
+        format!("All Episodes: {}", episodes),
+        format!("Status: {}", status), 
+        format!("Avg Score: {}", average_score), 
+        format!("Mean Score: {}", mean_score), 
+        format!("Popularity: {}", popularity), 
+        format!("Favourites: {}", favourites),
+        format!("Genres: {}", genres)
+        ].iter().map(|x| x.to_string()).collect(), 
+        vec![
+            title,
+            url, 
+            avatar, 
+            banner].iter().map(|x| x.to_string()).collect())
+
 }
--- src/commands/anilist.rs
+++ src/commands/anilist.rs
@@ -3,13 +3,23 @@ use serenity::framework::standard::{Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use crate::api::search_media::search;
+use tracing::info;
 
 #[command]
 pub async fn anime(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let media_name = args.message().to_string();
     let search = search(media_name, "ANIME".to_string()).await;
-
-    msg.reply(&ctx.http, search.join("\n")).await?;
+    info!("Information Received From Search Function\nVector 1: {}\n\nVector 2: {}", search.0.join("\n"), search.1.join("\n"));
+    
+    msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| { 
+            e.title(search.1.get(0).unwrap())
+            .url(search.1.get(1).unwrap())
+            .thumbnail(search.1.get(2).unwrap())
+            .image(search.1.get(3).unwrap())
+            .description(format!("{}", search.0.join("\n")))
+        })
+    }).await?;
 
     Ok(())
 }
